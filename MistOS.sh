a879cdfa95e3c91dfe156b9808176d60ffd782e7
Daffa Agusta <zaidanagusta319@gmail.com>
2024-10-24 13:15:09 +0700

    Update New

--- MistOS.sh
+++ MistOS.sh
@@ -3,23 +3,23 @@
 # Remove the local manifests directory if it exists (cleanup before repo initialization)
 rm -rf .repo/local_manifests/
 # Initialize ROM manifest
-repo init -u https://github.com/Evolution-X/manifest -b udc --git-lfs
+repo init -u https://github.com/Project-Mist-OS/manifest -b qpr3 --git-lfs
 # repo sync
 /opt/crave/resync.sh
 # cloning DT
 # device tree
-git clone https://github.com/EvolutionXOS/device_xiaomi_mojito.git --depth 1 -b mojito-universe device/xiaomi/mojito
-git clone https://github.com/EvolutionXOS/android_device_xiaomi_sm6150-common.git --depth 1 -b mojito-universe device/xiaomi/sm6150-common
+git clone https://github.com/Sepidermn/device_xiaomi_mojito.git --depth 1 -b mojito-universe device/xiaomi/mojito
+git clone https://github.com/Sepidermn/android_device_xiaomi_sm6150-common.git --depth 1 -b mojito-universe device/xiaomi/sm6150-common
 # kernel tree
-git clone https://github.com/EvolutionXOS/kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
+git clone https://github.com/Sepidermn/kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
 # vendor tree
-git clone https://gitlab.com/sachinbarange86/android_vendor_xiaomi_mojito.git --depth 1 -b 14 vendor/xiaomi/mojito
-git clone https://gitlab.com/sachinbarange86/android_vendor_xiaomi_sm6150-common.git --depth 1 -b 14 vendor/xiaomi/sm6150-common
+git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_mojito.git --depth 1 -b 14 vendor/xiaomi/mojito
+git clonehttps://gitlab.com/Sepidermn/android_vendor_xiaomi_sm6150-common.git --depth 1 -b 14 vendor/xiaomi/sm6150-common
 # hardware tree
-git clone https://github.com/EvolutionXOS/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
+git clone https://github.com/Sepidermn/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
 # set build environment
 . build/envsetup.sh
 # lunch
 lunch lineage_mojito-user
 # bacon
-m evolution
+make bacon
