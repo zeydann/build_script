b07d8a908d6cdd6e1b54782d8525465a043ff426
Sepidermn <zaidanagusta319@gmail.com>
2024-11-21 14:45:21 +0700

    script : RisingOS

---
+++ RisingOS.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Remove the local manifests directory if it exists (cleanup before repo initialization)
+rm -rf .repo/local_manifests/
+
+# remove device tree
+rm -rf device/xiaomi/mojito
+rm -rf device/xiaomi/sm6150-common
+rm -rf kernel/xiaomi/mojito
+rm -rf vendor/xiaomi/sm6150-common
+rm -rf hardware/xiaomi
+
+# Initialize ROM manifest
+repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
+
+# Sync the repo with force to ensure a clean sync
+/opt/crave/resync.sh
+
+# cloning device tree
+git clone https://github.com/Sepidermn/android_device_xiaomi_mojito.git --depth 1 -b 15 device/xiaomi/mojito
+git clone https://github.com/Sepidermn/android_device_xiaomi_sm6150-common.git --depth 1 -b 15 device/xiaomi/sm6150-common
+
+# cloning kernel tree
+git clone https://github.com/Sepidermn/android_kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
+
+# cloning vendor tree
+git clone https://gitlab.com/bliss-mojito/android_vendor_xiaomi_mojito.git --depth 1 -b 14 vendor/xiaomi/mojito
+git clone https://gitlab.com/bliss-mojito/android_vendor_xiaomi_sm6150-common.git --depth 1 -b 14 vendor/xiaomi/sm6150-common
+
+# cloning hardware tree
+git clone https://github.com/Sepidermn/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
+
+# Set up th build environment
+. build/envsetup.sh
+
+# Choose the target device
+riseup mojito userdebug
+
+# Build the ROM (use mka bacon for a full build)
+rise b
