ccc9f1da898ddc74546899499734a48e144f9d60
Sepidermn <zaidanagusta319@gmail.com>
2024-11-26 15:30:56 +0700

    script : RisingOS

--- script.sh
+++ script.sh
@@ -11,14 +11,14 @@ rm -rf vendor/xiaomi/sm6150-common
 rm -rf hardware/xiaomi
 
 # Initialize ROM manifest
-repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 15 --git-lfs
+repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
 
 # Sync the repo with force to ensure a clean sync
 /opt/crave/resync.sh
 
 # cloning device tree
-git clone https://github.com/Sepidermn/android_device_xiaomi_mojito.git --depth 1 -b derp device/xiaomi/mojito
-git clone https://github.com/Sepidermn/android_device_xiaomi_sm6150-common.git --depth 1 -b derp device/xiaomi/sm6150-common
+git clone https://github.com/Sepidermn/android_device_xiaomi_mojito.git --depth 1 -b 15 device/xiaomi/mojito
+git clone https://github.com/Sepidermn/android_device_xiaomi_sm6150-common.git --depth 1 -b 15 device/xiaomi/sm6150-common
 
 # cloning kernel tree
 git clone https://github.com/Sepidermn/android_kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
@@ -34,7 +34,7 @@ git clone https://github.com/Sepidermn/android_hardware_xiaomi.git --depth 1 -b 
 . build/envsetup.sh
 
 # Choose the target device
-lunch derp_mojito-userdebug
+riseup mojito userdebug
 
 # Build the ROM (use mka bacon for a full build)
-mka derp
+rise b
