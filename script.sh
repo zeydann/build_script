23d37c869077a0387be8c0162e7553946b0329cf
Sepidermn <zaidanagusta319@gmail.com>
2024-12-01 21:03:50 +0700

    add leica

--- script.sh
+++ script.sh
@@ -30,6 +30,9 @@ git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_sm6150-common.git -
 # cloning hardware tree
 git clone https://github.com/Sepidermn/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
 
+#add leica
+git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_mojito-leicacamera.git -b main vendor/xiaomi/mojito-leicacamera
+
 # Set up th build environment
 . build/envsetup.sh
 
