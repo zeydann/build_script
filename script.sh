b375eb1ef0b2e77611dd12e46c4d2e4f048a1d9c
Sepidermn <zaidanagusta319@gmail.com>
2025-01-07 22:53:15 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -21,7 +21,7 @@ git clone https://github.com/Sepidermn/android_device_xiaomi_mojito.git --depth 
 git clone https://github.com/Sepidermn/android_device_xiaomi_sm6150-common.git --depth 1 -b testing device/xiaomi/sm6150-common
 
 # cloning kernel tree
-git clone https://github.com/Kou-Yeager/kernel_xiaomi_mojito.git --depth 1 -b BORE+UCLAMP kernel/xiaomi/mojito
+git clone https://github.com/Sepidermn/android_kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
 
 # cloning vendor tree
 git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_mojito.git --depth 1 -b 15 vendor/xiaomi/mojito
