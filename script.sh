fff6911fdf126635fb640d864aed33f6938d1d14
Zeyy <zaidanagusta319@gmail.com>
2025-02-25 23:49:07 +0700

    test build

--- script.sh
+++ script.sh
@@ -11,17 +11,14 @@ rm -rf vendor/xiaomi/sm6150-common
 rm -rf hardware/xiaomi
 
 # Initialize ROM manifest
-repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
+repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
 
 # Sync the repo with force to ensure a clean sync
 /opt/crave/resync.sh
 
-# remove frameworks/native
-rm -rf frameworks/native
-
 # cloning device tree
-git clone https://github.com/zeydann/android_device_xiaomi_mojito.git --depth 1 -b evox device/xiaomi/mojito
-git clone https://github.com/zeydann/android_device_xiaomi_sm6150-common.git --depth 1 -b mojito device/xiaomi/sm6150-common
+git clone https://github.com/zeydann/android_device_xiaomi_mojito.git --depth 1 -b matrix device/xiaomi/mojito
+git clone https://github.com/zeydann/android_device_xiaomi_sm6150-common.git --depth 1 -b test device/xiaomi/sm6150-common
 
 # cloning kernel tree
 git clone https://github.com/zeydann/kernel_xiaomi_mojito.git --depth 1 -b 15 kernel/xiaomi/mojito
@@ -33,9 +30,6 @@ git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_sm6150-common.git -
 # cloning hardware tree
 git clone https://github.com/zeydann/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
 
-# add modify
-git clone https://github.com/zeydann/frameworks_native.git --depth 1 -b vic frameworks/native
-
 # Export
 export SELINUX_IGNORE_NEVERALLOWS=true
 
@@ -43,7 +37,4 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 . build/envsetup.sh
 
 # Choose the target device
-lunch lineage_mojito-ap4a-userdebug
-
-# Build the ROM (use mka bacon for a full build)
-m evolution
+brunch mojito userdebug
