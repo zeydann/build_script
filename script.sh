ff9138edf370caa5d570f776e3d0c2f6f8f71546
Zeyy <zaidanagusta319@gmail.com>
2025-03-03 15:14:03 +0700

    test build

--- script.sh
+++ script.sh
@@ -16,12 +16,9 @@ repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
 # Sync the repo with force to ensure a clean sync
 /opt/crave/resync.sh
 
-# remove frameworks/native
-rm -rf frameworks/native
-
 # cloning device tree
 git clone https://github.com/zeydann/android_device_xiaomi_mojito.git --depth 1 -b matrix device/xiaomi/mojito
-git clone https://github.com/zeydann/android_device_xiaomi_sm6150-common.git --depth 1 -b 15 device/xiaomi/sm6150-common
+git clone https://github.com/zeydann/android_device_xiaomi_sm6150-common.git --depth 1 -b mojito device/xiaomi/sm6150-common
 
 # cloning kernel tree
 git clone https://github.com/zeydann/kernel_xiaomi_mojito.git --depth 1 -b 15 kernel/xiaomi/mojito
@@ -32,9 +29,6 @@ git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_sm6150-common.git -
 
 # cloning hardware tree
 git clone https://github.com/zeydann/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
-
-# add frameworks/native
-git clone https://github.com/zeydann/android_frameworks_native.git --depth 1 -b 15 frameworks/native
 
 # Export
 export SELINUX_IGNORE_NEVERALLOWS=true
