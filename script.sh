03bd132d51bd1fdd086676b14e90bd93fbf7a29a
Zeyy <zaidanagusta319@gmail.com>
2025-03-03 08:29:24 +0700

    test build

--- script.sh
+++ script.sh
@@ -11,7 +11,7 @@ rm -rf vendor/xiaomi/sm6150-common
 rm -rf hardware/xiaomi
 
 # Initialize ROM manifest
-repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
+repo init -u https://github.com/RisingOS-Revived/android -b fifteen --git-lfs
 
 # Sync the repo with force to ensure a clean sync
 /opt/crave/resync.sh
@@ -20,7 +20,7 @@ repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
 rm -rf frameworks/native
 
 # cloning device tree
-git clone https://github.com/zeydann/android_device_xiaomi_mojito.git --depth 1 -b evox device/xiaomi/mojito
+git clone https://github.com/zeydann/android_device_xiaomi_mojito.git --depth 1 -b rising device/xiaomi/mojito
 git clone https://github.com/zeydann/android_device_xiaomi_sm6150-common.git --depth 1 -b mojito device/xiaomi/sm6150-common
 
 # cloning kernel tree
@@ -33,9 +33,6 @@ git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_sm6150-common.git -
 # cloning hardware tree
 git clone https://github.com/zeydann/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
 
-# add modify
-git clone https://github.com/zeydann/frameworks_native.git --depth 1 -b vic frameworks/native
-
 # Export
 export SELINUX_IGNORE_NEVERALLOWS=true
 
@@ -43,7 +40,10 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 . build/envsetup.sh
 
 # Choose the target device
-lunch lineage_mojito-ap4a-userdebug
+riseup mojito userdebug
+
+# keys
+gk -s
 
 # Build the ROM (use mka bacon for a full build)
-m evolution
+rise b
