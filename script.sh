066c8c2a23d256a966750af21c75c784e2564b46
Zeyy <zaidanagusta319@gmail.com>
2025-03-09 20:54:43 +0700

    test build

--- script.sh
+++ script.sh
@@ -16,12 +16,9 @@ repo init -u https://github.com/RisingOS-Revived/android -b fifteen --git-lfs
 # Sync the repo with force to ensure a clean sync
 /opt/crave/resync.sh
 
-# remove frameworks/native
-rm -rf frameworks/native
-
 # cloning device tree
 git clone https://github.com/zeydann/android_device_xiaomi_mojito.git --depth 1 -b rising device/xiaomi/mojito
-git clone https://github.com/zeydann/android_device_xiaomi_sm6150-common.git --depth 1 -b mojito device/xiaomi/sm6150-common
+git clone https://github.com/zeydann/android_device_xiaomi_sm6150-common.git --depth 1 -b 15 device/xiaomi/sm6150-common
 
 # cloning kernel tree
 git clone https://github.com/zeydann/kernel_xiaomi_mojito.git --depth 1 -b 15 kernel/xiaomi/mojito
