b27109a45e222e70b9f31aa332eed7aa298c4c4e
Zeyy <zaidanagusta319@gmail.com>
2025-03-16 09:45:24 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -28,7 +28,7 @@ git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_mojito.git --depth 
 git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_sm6150-common.git --depth 1 -b 15 vendor/xiaomi/sm6150-common
 
 # cloning hardware tree
-git clone https://github.com/zeydann/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
+git clone https://github.com/zeydann/android_hardware_xiaomi.git --depth 1 -b rising hardware/xiaomi
 
 # Export
 export SELINUX_IGNORE_NEVERALLOWS=true
